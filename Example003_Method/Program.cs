d731134d7aad4b7a988875f972d0c2a14f472b03
MABp <mabp-man@k66.ru>
2022-10-06 02:42:19 +0500

    проверка ввода числа

---
+++ Example003_Method/Program.cs
@@ -0,0 +1,9 @@
+﻿double varA;
+while (true)
+{
+    Console.WriteLine("Введите число: ");
+    if(double.TryParse(Console.ReadLine(), out varA)) break;
+    Console.WriteLine("Ошибка ввода!");
+}
+
+Console.WriteLine($"A={varA}");
