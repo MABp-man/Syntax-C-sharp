085c15c891e63aba4788d17dee2aaf994b364e3f
MABp <mabp-man@k66.ru>
2022-10-07 01:27:04 +0500

    dz 10 done

--- DZ10/Program.cs
+++ DZ10/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int varA;
+while (true)
+{
+    Console.Write("Введите трехзначное число: ");
+    if(int.TryParse(Console.ReadLine(), out varA)) 
+        if(varA > 99 && varA < 1000) break;
+    Console.WriteLine("Ошибка ввода!");
+} 
+Console.WriteLine($"{varA} => {varA % 100 / 10}");
