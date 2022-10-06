6eb897db916aa284621d2877aebdd878df3ffb51
MABp <mabp-man@k66.ru>
2022-10-07 01:55:43 +0500

    dz 13 done

--- DZ13/Program.cs
+++ DZ13/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int varA;
+while (true)
+{
+    Console.Write("Введите целое число: ");
+    if(int.TryParse(Console.ReadLine(), out varA)) break;
+    Console.WriteLine("Ошибка ввода!");
+} 
+Console.Write($"{varA} => ");
+
+if(varA < 100) 
+    Console.WriteLine("третьей цифры нет");
+else
+{
+    while (varA > 999)
+    {
+        varA = varA / 10;
+    }
+    Console.WriteLine(varA % 10);
+}
