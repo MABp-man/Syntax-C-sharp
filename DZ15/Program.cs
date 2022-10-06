3ca967dacbba1886fae57878537d21aabcc739d1
MABp <mabp-man@k66.ru>
2022-10-07 02:08:23 +0500

    dz 15 done

--- DZ15/Program.cs
+++ DZ15/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int varA;
+while (true)
+{
+    Console.Write("Введите номер дня недели: ");
+    if(int.TryParse(Console.ReadLine(), out varA)) 
+        if(varA < 8 && varA > 0) break;
+    Console.WriteLine("Ошибка ввода!");
+} 
+Console.Write(varA);
+if (varA > 5)
+    Console.WriteLine(" => да");
+else
+    Console.WriteLine(" => нет");
