115c065c5716befa20a56a8be2d34979f2b1fc03
MABp <mabp-man@k66.ru>
2022-10-08 16:18:12 +0500

    seminar 08.10 first task

---
+++ Seminar_0810/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Clear();
+int quarter;
+
+while (true)
+{
+    Console.WriteLine("Введите номер четверти системы координат: ");
+    if(int.TryParse(Console.ReadLine(), out quarter)) 
+        if (quarter < 5 && quarter > 0) break;
+    Console.Clear();
+    Console.WriteLine("Ошибка ввода!");
+}
+
+QuarterRange(quarter);
+
+
+static void QuarterRange(int quarter)
+{
+if (quarter == 1)
+    Console.WriteLine("X > 0, Y > 0");
+else if (quarter == 2)
+        Console.WriteLine("X < 0, Y > 0");
+    else if (quarter == 3)
+            Console.WriteLine("X < 0, Y < 0");
+        else if (quarter == 4)
+             Console.WriteLine("X > 0, Y > 0");
+}
