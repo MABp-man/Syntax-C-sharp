41d7d8ae35c85010dd2b6289ccedc44d88f4c0ca
MABp <mabp-man@k66.ru>
2022-10-08 14:22:04 +0500

    try-cath block added

---
+++ TryCatch/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.clear();
+int = x, y;
+
+try
+{
+    Console.Write("Введите координату X: ");
+    x = int.Parse(Console.Readline() ?? "");
+
+    Console.Write("Введите координату Y: ");
+    y = int.Parse(Console.Readline() ?? "");
+}
+catch (Exception exc)
+{
+    Console.Writeline($"Ошибка ввода данных: {exc.Message}");
+    return;
+}
