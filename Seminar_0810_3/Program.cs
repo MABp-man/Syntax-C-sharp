8100def8df83ba4977365095a548570a3196e9bd
MABp <mabp-man@k66.ru>
2022-10-08 17:22:22 +0500

    seminar 08.10 second and third tasks

---
+++ Seminar_0810_3/Program.cs
@@ -0,0 +1,21 @@
+﻿static void PrintSquares(int N)
+{
+    Console.Write($"{N} -> ");
+    for (int i = 1; i < N; i++)
+    {
+        Console.Write($"{i*i}, ");
+    }
+    Console.Write(N*N);
+}
+
+/////////////////////////
+
+int N;
+while (true)
+{
+    Console.WriteLine("Введите целое число: ");
+    if(int.TryParse(Console.ReadLine(), out N)) break;
+    Console.WriteLine("Ошибка ввода!");
+}
+
+PrintSquares(N);
---
+++ seminar_0810_2_method_2/Program.cs
@@ -0,0 +1,22 @@
+﻿static int CoordinateInput(string coordinates)
+{
+    Console.Write(coordinates);
+    int coords = int.Parse(Console.ReadLine() ?? "");
+    return coords;
+}
+
+static double DistanceCount(int X1, int Y1, int X2, int Y2)
+{
+    double distance = Math.Sqrt(Math.Pow(X2 - X1, 2) + Math.Pow(Y2 - Y1, 2));
+    return distance;
+}
+
+int X1, Y1, X2, Y2;
+
+X1 = CoordinateInput("Введите координату X первой точки: ");
+Y1 = CoordinateInput("Введите координату Y первой точки: ");
+X2 = CoordinateInput("Введите координату X второй точки: ");
+Y2 = CoordinateInput("Введите координату Y второй точки: ");
+
+double distance = DistanceCount(X1, Y1, X2, Y2);
+Console.WriteLine(distance);
