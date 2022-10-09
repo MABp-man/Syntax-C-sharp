1c23dfcace1521781e47ffc6a1fff3125eef7b29
MABp <mabp-man@k66.ru>
2022-10-09 13:17:03 +0500

    DZ19 done

---
+++ DZ19/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.Clear();
+int fiveDigitN = FiveDigitInput("Введите пятизначное число: ");
+PalindromCheck(ref fiveDigitN);
+
+/////////////////////
+
+static int FiveDigitInput(string Welcome)
+{
+int fiveDigitN;
+while (true)
+{
+    Console.WriteLine(Welcome);
+    if(int.TryParse(Console.ReadLine(), out fiveDigitN)) 
+        if (fiveDigitN < 100000 && fiveDigitN > 9999) break;
+    Console.Clear();
+    Console.WriteLine("Ошибка ввода!");
+}
+return fiveDigitN;
+}
+/////////////////
+
+static void PalindromCheck(ref int fiveDigitN)
+{
+    if (fiveDigitN / 10000 == fiveDigitN % 10 && (fiveDigitN / 1000) % 10 == (fiveDigitN % 100) / 10)
+        Console.WriteLine(fiveDigitN + " => палиндром");
+    else
+        Console.WriteLine(fiveDigitN + " => НЕ палиндром");
+}
