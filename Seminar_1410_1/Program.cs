0cbf1f1e482673ba679e7a2e4f3f5c831b384ab6
MABp <mabp-man@k66.ru>
2022-10-15 10:48:50 +0500

    seminar 1410 sumNumber done

--- Seminar_1410_1/Program.cs
+++ Seminar_1410_1/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+int N = NumberInput("Введите целое положительное число: ","Ошибка ввода!");
+Console.WriteLine($"{N} => {GetSumN(N)}");
+
+/////////////////////
+
+static int NumberInput(string WelcomeMessage, string ErrorMessage)
+{
+    while (true)
+    {
+        Console.WriteLine(WelcomeMessage);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userN);
+        if (isCorrect)
+          return userN;
+        Console.Clear();
+        Console.WriteLine(ErrorMessage);
+    }
+}
+/////////////////
+
+static int GetSumN(int N)
+{
+    int sumN = 0;
+    for (int i = N; i > 0; i--)
+    {
+        sumN = sumN + i;
+    }
+    return sumN;
+}
