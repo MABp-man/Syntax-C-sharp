ddea650af5918b3a29eedf42e857026a29709c1c
MABp <mabp-man@k66.ru>
2022-10-15 18:28:07 +0500

    DZ 29 write to console part is done

---
+++ DZ29/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+int[] NewArray = {1, 2, 5, 7, 19};
+
+Console.Write (WriteArrayToConsole(NewArray));
+
+
+////////////////////
+static string WriteArrayToConsole(int[] NumbersArray)
+{
+    Console.Write("[");
+    for (int i = 0; i < NumbersArray.Length - 1; i++)
+    {
+        Console.Write($"{NumbersArray[i]}, ");
+    }
+    string writeArray = $"{NumbersArray[NumbersArray.Length - 1]}]";
+    return writeArray;
+}
