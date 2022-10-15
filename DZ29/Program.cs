57249b6c5a12a8e0c97feeb77bb5771a156433d2
MABp <mabp-man@k66.ru>
2022-10-16 01:31:00 +0500

    DZ 29 done

--- DZ29/Program.cs
+++ DZ29/Program.cs
@@ -2,9 +2,43 @@
 // 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 // 6, 1, 33 -> [6, 1, 33]
 
-int[] NewArray = {1, 2, 5, 7, 19};
-
+int[] NewArray = GetArrayFromUser("Введите размер массива: ", "Введите целое число в ячейку");
+Console.Clear();
+Console.Write ("Ваш массив создан: ");
 Console.Write (WriteArrayToConsole(NewArray));
+
+////////////////////
+
+static int[] GetArrayFromUser(string WelcomeMessage1, string WelcomeMessage2)
+{
+    Console.Clear();
+    int size, Number;
+    while (true)
+    {
+            Console.Write(WelcomeMessage1);
+            if(int.TryParse(Console.ReadLine(), out size)) break;
+            Console.WriteLine("Ошибка ввода!");
+    }
+    
+    int[] array = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        while (true)
+        {
+            Console.Write($"{WelcomeMessage2} номер {i+1}: ");
+            if(int.TryParse(Console.ReadLine(), out Number)) 
+            {
+                array[i] = Number;
+                break;
+            }
+            Console.WriteLine("Ошибка ввода!");
+        }
+        
+    
+    }
+    return array;
+}
 
 
 ////////////////////
@@ -16,5 +50,6 @@ static string WriteArrayToConsole(int[] NumbersArray)
         Console.Write($"{NumbersArray[i]}, ");
     }
     string writeArray = $"{NumbersArray[NumbersArray.Length - 1]}]";
+
     return writeArray;
 }
