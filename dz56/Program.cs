dc145672eae4cc07b0bd70b52c546068443dafd2
MABp <mabp-man@k66.ru>
2022-11-12 15:20:40 +0500

    dz 56 restored

--- dz56/Program.cs
+++ dz56/Program.cs
@@ -1,2 +1,73 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте прямоугольный двумерный массив. Напишите программу,
+// которая будет находить строку с наименьшей суммой элементов.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Программа считает сумму элементов в каждой строке
+// и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+Console.Clear();
+// int[,] newArray = {{1, 4, 7 ,2}, {5, 9, 2, 3}, {8, 4, 2, 4}, {5, 2, 6, 7}};  // строка для проверки
+int[,] newArray = NewRandom2DArray(4, 5, -100, 101);
+Print2DArray(newArray);
+Console.WriteLine();
+FindMinRowSum(newArray);
+
+////////////////////////
+
+static void FindMinRowSum(int[,] array)
+    {
+        int min = RowSum(array, 0);
+        int indexMin = 0;
+        for (int i = 1; i < array.GetLength(0); i++)
+        {        
+            if (RowSum(array, i) < min)
+               {
+                    min = RowSum(array, i);
+                    indexMin = i;
+               }
+        }
+        Console.Write("Строка с минимальной суммой элементов: ");
+        Console.WriteLine($"{indexMin + 1} ({RowSum(array, indexMin)})");
+    }
+
+///////////////////////
+
+static int RowSum(int[,] array, int row)
+    {
+        int sum = 0;
+        for (int i = 0; i < array.GetLength(1); i++)
+            sum = sum + array[row, i];            
+        return sum;
+    } 
+
+////////////////////////
+
+static int[,] NewRandom2DArray(int rows, int columns, int from, int to)
+    {
+       int[,] array = new int[rows, columns];
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                array [i, j] = new Random().Next(from, to);
+            }
+        }
+        return array;
+    }
+
+///////////////////////
+
+static void Print2DArray(int[,] array)
+    {
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            Console.WriteLine();
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                Console.Write($"{array[i, j]}  ");
+            }
+        }
+    }
